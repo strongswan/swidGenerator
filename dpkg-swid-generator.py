373c13f9f6b0aeee6e56945d3801fffe131e5735
Jonas Furrer <jonas@sequenz.net>
2014-03-08 21:08:49 +0100

    Changes according to dbrgn's input

--- dpkg-swid-generator.py
+++ dpkg-swid-generator.py
@@ -7,86 +7,86 @@ import platform
 
 
 class PackageInfo(object):
-	package = ''
-	version = ''
+    package = ''
+    version = ''
 
 
 class DpkgEnvironment(object):
+    command_args = ["dpkg-query", "-W", "-f=${Package}\\t${Version}\\n"]
 
-	command_args = ["dpkg-query", "-W", "-f=${Package}\\t${Version}\\n"]
+    @staticmethod
+    def get_list(data):
+        line_list = data.split('\n')
 
-	def get_list(self, data):
-		line_list = data.split('\n')
+        result = []
 
-		result = []
+        for line in line_list:
+            split_line = line.split('\t')
+            if len(split_line) == 2:
+                info = PackageInfo()
+                info.package = split_line[0]
+                info.version = split_line[1]
 
-		for line in line_list:
-			split_line = line.split('\t')
-			if(len(split_line) == 2):
-				info = PackageInfo()
-				info.package = split_line[0]
-				info.version = split_line[1]
+                result.append(info)
 
-				result.append(info)
+        return result
 
-		return result
-
-	def get_os_string(self):
-		dist = platform.dist()
-		return dist[0] + '_' + dist[1]
+    @staticmethod
+    def get_os_string():
+        dist = platform.dist()
+        return dist[0] + '_' + dist[1]
 
 
 class OutputGenerator(object):
+    regid = "regid.2004-03.org.strongswan"
+    role = "tagcreator"
+    version_scheme = "alphanumeric"
+    xmlns = "http://standards.iso.org/iso/19770/-2/2014/schema.xsd"
 
-	regid = "regid.2004-03.org.strongswan"
-	role = "tagcreator"
-	version_scheme = "alphanumeric"
-	xmlns = "http://standards.iso.org/iso/19770/-2/2014/schema.xsd"
+    def __init__(self, environment):
+        self.environment = environment
 
-	def __init__(self, environment):
-		self.environment = environment
+    def _call_package_manager(self):
+        self.raw_output = subprocess.check_output(self.environment.command_args)
 
-	def __call_package_manager(self):
-		self.raw_output = subprocess.check_output(self.environment.command_args)
+    def _get_list(self):
+        self._call_package_manager()
+        return self.environment.get_list(self.raw_output)
 
-	def __get_list(self):
-		self.__call_package_manager()
-		return self.environment.get_list(self.raw_output)
+    def _get_os_string(self):
+        return self.environment.get_os_string()
 
-	def __get_os_string(self):
-		return self.environment.get_os_string()
+    def create_swid_tags(self):
+        pkg_info = self._get_list()
+        os_info = self._get_os_string()
 
-	def create_swid_tags(self):
-		pkg_info = self.__get_list()
-		os_info = self.__get_os_string()
+        swidtags = []
 
-		swidtags = []
+        for pi in pkg_info:
+            software_identity = ET.Element("SoftwareIdentity")
+            software_identity.set('xmlns', self.xmlns)
+            software_identity.set('name', pi.package)
+            software_identity.set('uniqueId', '{os_info}-{pi.package}-{pi.version}'.format(os_info=os_info, pi=pi))
+            software_identity.set('version', pi.version)
+            software_identity.set('versionScheme', self.version_scheme)
 
-		for pi in pkg_info:
-			softwareIdentity = ET.Element("SoftwareIdentity")
-			softwareIdentity.set('xmlns', OutputGenerator.xmlns)
-			softwareIdentity.set('name', pi.package)
-			softwareIdentity.set('uniqueId', os_info + '-' + pi.package + '-' + pi.version)
-			softwareIdentity.set('version', pi.version)
-			softwareIdentity.set('versionScheme', OutputGenerator.version_scheme)
+            entity = ET.SubElement(software_identity, "Entity")
+            entity.set('name', pi.package)
+            entity.set('regid', self.regid)
+            entity.set('role', self.role)
 
-			entity = ET.SubElement(softwareIdentity, "Entity")
-			entity.set('name', pi.package)
-			entity.set('regid', OutputGenerator.regid)
-			entity.set('role', OutputGenerator.role)
+            swidtag_flat = ET.tostring(software_identity, 'UTF-8')
+            swidtag_reparsed = minidom.parseString(swidtag_flat)
 
-			swidtag_flat = ET.tostring(softwareIdentity, 'UTF-8')
-			swidtag_reparsed = minidom.parseString(swidtag_flat)
+            swidtags.append(swidtag_reparsed.toprettyxml(indent="\t", encoding='UTF-8'))
 
-			swidtags.append(swidtag_reparsed.toprettyxml(indent="\t", encoding='UTF-8'))
-
-		return '\n'.join(swidtags)
+        return '\n'.join(swidtags)
 
 
 def main():
-	dpkg_env = DpkgEnvironment()
-	gen = OutputGenerator(dpkg_env)
-	print gen.create_swid_tags()
+    dpkg_env = DpkgEnvironment()
+    gen = OutputGenerator(dpkg_env)
+    print gen.create_swid_tags()
 
 
 if __name__ == '__main__':
