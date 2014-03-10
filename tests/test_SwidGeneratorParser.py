4d410392517df07e1bb46a23064628edec6b45ce
cfaessler <cfaessle@hsr.ch>
2014-03-10 17:23:44 +0100

    look @dbrgn, i added fixtures only for you

--- tests/test_SwidGeneratorParser.py
+++ tests/test_SwidGeneratorParser.py
@@ -7,22 +7,24 @@ from swidGenerator.settings import DEFAULT_REGID
 from argparse import ArgumentTypeError
 
 
-def test_without_arguments():
-    parser = SwidGeneratorArgumentParser()
+@pytest.fixture
+def parser():
+    return SwidGeneratorArgumentParser()
+
+
+def test_without_arguments(parser):
     result = parser.parse([])
     assert result.full is False
     assert result.regid == DEFAULT_REGID
 
 
-def test_tag_creator():
-    parser = SwidGeneratorArgumentParser()
+def test_tag_creator(parser):
     test_creator = 'hsr.ch'
     result = parser.parse(('--regid=' + test_creator).split())
     assert result.regid == test_creator
 
 
-def test_full_argument():
-    parser = SwidGeneratorArgumentParser()
+def test_full_argument(parser):
     result = parser.parse('--full'.split())
     assert result.full is True
 
@@ -32,7 +34,6 @@ def test_invalid_regid_format():
         regid_string('09.strongswan.org*')
 
 
-def test_pretty_parameter():
-    parser = SwidGeneratorArgumentParser()
+def test_pretty_parameter(parser):
     result = parser.parse('--pretty'.split())
     assert result.pretty == True
