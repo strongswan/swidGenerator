91b13dc8c25bdc544080431313c3075a085afaae
Jonas Furrer <jonas@sequenz.net>
2014-03-15 14:41:19 +0100

    tnc-ba/strongTNC#39 changed regex for regid and entity name

--- swidGenerator/swidgenerator_argumentparser.py
+++ swidGenerator/swidgenerator_argumentparser.py
@@ -5,11 +5,20 @@ from swidGenerator.settings import DEFAULT_REGID, DEFAULT_ENTITY_NAME
 import re
 
 
-def regid_entity_name_string(string):
+def regid_string(string):
     if string is None:
         return None
     try:
-        return re.match('^[\w|\.\-]*$', string).group(0)
+        return re.match('^regid\.\d{4}-\d{2}\.[^ /|:<>*?&\\\]*$', string).group(0)
+    except:
+        raise ArgumentTypeError('String \'{0}\' does not match required format'.format(string))
+
+
+def entity_name_string(string):
+    if string is None:
+        return None
+    try:
+        return re.match('^[^<&"]*$', string).group(0)
     except:
         raise ArgumentTypeError('String \'{0}\' does not match required format'.format(string))
 
@@ -28,12 +37,12 @@ class SwidGeneratorArgumentParser(object):
                                 help='Dumps the full SWID tags including file tags for each package')
         arg_parser.add_argument('--pretty', action='store_true', default=False,
                                 help='Generate pretty readable output')
-        arg_parser.add_argument('--regid', dest='regid', type=regid_entity_name_string,
-                                default=regid_entity_name_string(DEFAULT_REGID),
+        arg_parser.add_argument('--regid', dest='regid', type=regid_string,
+                                default=regid_string(DEFAULT_REGID),
                                 help='Specify the regid value (used in the <Entity> tag for the regid attribute).'
                                      'Shall not contain any whitespace characters')
-        arg_parser.add_argument('--entity-name', dest='entity_name', type=regid_entity_name_string,
-                                default=regid_entity_name_string(DEFAULT_ENTITY_NAME),
+        arg_parser.add_argument('--entity-name', dest='entity_name', type=entity_name_string,
+                                default=entity_name_string(DEFAULT_ENTITY_NAME),
                                 help='Specify the entity name (used in the <Entity> tag for the name attribute).'
                                      'Shall not contain any whitespace characters')
         arg_parser.add_argument('environment', choices=['dpkg', 'yum'],
