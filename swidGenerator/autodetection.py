74b5e2b974b38f5b8d0a8f80904d2427613fe378
cfaessler <cfaessle@hsr.ch>
2014-03-15 16:51:52 +0100

    tnc-ba/strongTNC#43 implemented executable lookup in path, thx @d22 good idea

--- swidGenerator/autodetection.py
+++ swidGenerator/autodetection.py
@@ -1,15 +1,20 @@
 import os.path
 from swidgenerator import DpkgEnvironment, YumEnvironment
+from distutils.spawn import find_executable
 
 
 def autodetect_env():
+    """
+    Tries to detect the used packetmanager by searching for a given executable in the path.
+    Returns a static class representing the packetmanager environment or None if autodetection fails.
+    """
     envs = {
-        DpkgEnvironment: '/usr/bin/dpkg-query',
-        YumEnvironment: '/usr/bin/yum'
+        DpkgEnvironment: 'dpkg-query',
+        YumEnvironment: 'yum'
     }
 
     for environment_string, path in envs.iteritems():
-        if os.path.isfile(path):
+        if find_executable(path):
             return environment_string
 
     return None
