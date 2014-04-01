97074355bcf61dbe29e04f6b6bb5ce8190f00f6f
Danilo Bargen <mail@dbrgn.ch>
2014-04-02 00:37:08 +0200

    Added setup.py to make the SWID generator easily installable
    The swid_generator script can now be installed to your local PYTHONPATH
    with ::
    
        python setup.py install
    
    To install only symlinks to the sourcefiles (which means that code
    changes are immediately reflected in the installation), use ::
    
        pip install -e .
    
    Refs tnc-ba/strongTNC#40.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from swid_generator import meta
+
+readme = open('README.rst').read()
+
+setup(name='swid_generator',
+      version=meta.version,
+      description=meta.description,
+      author=meta.authors,
+      url='https://github.com/tnc-ba/swidGenerator',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      license=meta.license,
+      keywords='swid dpkg yum tnc',
+      long_description=readme,
+      entry_points={
+          'console_scripts': [
+              '%s = swid_generator.main:main' % meta.title,
+          ]
+      },
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+      ],
+)
---
+++ swid_generator/meta.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""This module contains metadata about the project."""
+
+title = 'swid_generator'
+version = '0.0.1'
+description = 'Application which generates SWID-Tags from Linux installed packages, using tools as dpgk or yum.'
+authors = 'Christian Fässler, Danilo Bargen, Jonas Furrer'
+license = 'GPLv3'
