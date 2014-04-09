e508d5fe19910939861b19a355ff0fc4b75aefc3
Danilo Bargen <mail@dbrgn.ch>
2014-04-10 00:33:19 +0200

    Switched license to MIT
    - Updated information in setup.py
    - Added LICENSE.txt
    - Added license information to `swid_generator/main.py` and `README.rst`
    
    Fixes tnc-ba/strongTNC#97.

--- swid_generator/meta.py
+++ swid_generator/meta.py
@@ -4,6 +4,6 @@
 title = 'swid_generator'
 version = '0.0.1'
 description = 'Application which generates SWID-Tags from Linux installed packages, ' \
-              'using tools as dpgk or rpm.'
+              'using tools like DPGK or RPM.'
 authors = 'Christian Fässler, Danilo Bargen, Jonas Furrer'
-license = 'GPLv3'
+license = 'MIT'
