d5945bb5ac885a0032f1250b1e72781048fcd2fc
Davide De Giorgio <dave_degiorgio@hotmail.com>
2017-03-21 15:15:24 +0100

    PackageInfo class adjusted for fullpath-splitting

--- swid_generator/package_info.py
+++ swid_generator/package_info.py
@@ -6,13 +6,27 @@ import os.path
 
 class FileInfo(object):
     def __init__(self, path):
-        self.location, self.name = os.path.split(path)
+        self.name = (os.path.split(path)[1]).split(' ')[0]
+        self.location = os.path.split(path)[0]
+        self.mutable = False
+        self.fullpathname = self.location + "/" + self.name
+
+        splitted_location = self.location.split('/')
+        splitted_location.append(self.name)
+
+        self.fullpathname_splitted = splitted_location[1:len(splitted_location)]
 
 
 class PackageInfo(object):
-    def __init__(self, package='', version='', files=None):
+    def __init__(self, package='', version='', files=None, status=None):
         if files is None:
             files = []
         self.package = package
         self.version = version
         self.files = files
+        self.status = status
+        self.files_structured = []
+
+    def append_file(self, file_info):
+        self.files.append(file_info)
+        self.files_structured.append(file_info.fullpathname_splitted)
