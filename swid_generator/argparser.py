66a49f04cae373c15245734c05a8c5f99d75f08d
Christof Greiner <cgreiner@hsr.ch>
2017-04-04 15:14:18 +0200

    Solved TODO regid regex

--- swid_generator/argparser.py
+++ swid_generator/argparser.py
@@ -21,8 +21,13 @@ def regid_string(string):
     if string is None:
         return None
     try:
-        # TODO: Replace with new Regid Regex
-        return re.match(r'strongswan.org', string).group(0)
+        regex = re.compile(
+            r'^(?:(?:http|ftp)s?://)?'
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+            r'(?::\d+)?'
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        return re.match(regex, string).group(0)
     except:
         raise ArgumentTypeError("String '{0}' does not match required format".format(string))
 
