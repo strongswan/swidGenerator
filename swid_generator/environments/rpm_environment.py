adfffa24039fd0023039ef206e32dda4bc23fb9a
Davide De Giorgio <dave_degiorgio@hotmail.com>
2017-04-10 06:49:54 +0200

    RPM-Environment for Package-Query functionality prepared; get_packge_info and get_files_from_package Methods added and partially implemented

--- swid_generator/environments/rpm_environment.py
+++ swid_generator/environments/rpm_environment.py
@@ -17,6 +17,8 @@ class RpmEnvironment(CommonEnvironment):
 
     """
     executable = 'rpm'
+    CONFFILE_FILE_NAME = 'conffiles'
+    CONTROL_ARCHIVE = 'control.tar.gz'
 
     @classmethod
     def get_package_list(cls):
@@ -74,3 +76,65 @@ class RpmEnvironment(CommonEnvironment):
                 result_list.append(FileInfo(path))
 
         return result_list
+
+    @classmethod
+    def get_files_from_packagefile(cls, file_path):
+
+        def _run_info_query_command(file_type):
+            command_args = [cls.executable, "--query", "--package", file_path, file_type, file_path]
+            output = subprocess.check_output(command_args)
+            if isinstance(output, bytes):
+                output = output.decode('utf-8')
+
+            return output
+
+        all_file_info = []
+        normal_files = filter(lambda fp: len(fp) > 0, _run_info_query_command("-l").split('\n'))
+        config_files = filter(lambda fp: len(fp) > 0, _run_info_query_command("-c").split('\n'))
+
+        save_options = cls._create_temp_folder(file_path)
+
+        # rpm2cpio zsh-5.1.1-4.fc23.x86_64.rpm | cpio -id --quiet
+
+        """
+            ps = subprocess.Popen(('ps', '-A'), stdout=subprocess.PIPE)
+            output = subprocess.check_output(('grep', 'process_name'), stdin=ps.stdout)
+            ps.wait()
+        """
+
+        rpm2cpio = subprocess.Popen(["rpm2cpio", file_path], stdout=subprocess.PIPE)
+        output = subprocess.check_output(["cpio", "-id", "--quiet"], stdin=rpm2cpio.stdout)
+
+        print(output)
+
+
+        for file_path in normal_files:
+            if cls._is_file(file_path[1:len(file_path)]):
+                file_info = FileInfo(file_path)
+                all_file_info.append(file_info)
+
+        for file_path in config_files:
+            if cls._is_file(file_path):
+                file_info = FileInfo(file_path)
+                file_info.mutable = True
+                all_file_info.append(file_info)
+
+        return all_file_info
+
+
+    @classmethod
+    def get_packageinfo_from_packagefile(cls, file_path):
+
+        def _run_info_query_command(field):
+            command_args = [cls.executable, "--query", "--package", "--queryformat", "%{" + field + "}",
+                            file_path]
+            output = subprocess.check_output(command_args)
+            if isinstance(output, bytes):
+                output = output.decode('utf-8')
+            return output
+
+        package_info = PackageInfo()
+        package_info.package = _run_info_query_command("name")
+        package_info.version = _run_info_query_command("version")
+
+        return package_info
