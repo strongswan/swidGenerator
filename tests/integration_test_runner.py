1ae7c525fff01c0df1fd087d911deaff61a39c5a
degiorgioo <dave_degiorgio@hotmail.com>
2017-05-30 08:52:17 +0200

    IntegrationTestRunner restructured and Docstrings added

--- tests/integration_test_runner.py
+++ tests/integration_test_runner.py
@@ -1,27 +1,18 @@
+# -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
+
 import subprocess
 import sys
 
 
-def _execute_command(cmd):
-    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
-    for stdout_line in iter(popen.stdout.readline, ""):
-        yield stdout_line
-    popen.stdout.close()
-    return_code = popen.wait()
-    if return_code:
-        sys.exit(return_code)
-
-
-def _compose_test_files(test_files):
-    result = ""
-    for file_name in test_files:
-        result += " " + '/'.join(("tests", file_name))
-    return result.lstrip()
-
-
 class IntegrationTestRunner(object):
-    # Command separated with commas because of split in docker_command_args variable and ENV variable composition
+    """
+    Class to run the integration tests on docker-containers.
+    The run_main method, builds the docker-command for each environment and Python version and starts a subprocess.
+
+    """
+
+    # Command separated with commas because of split in TOX_FILES and TOXENV variable
     docker_command = "docker,run,-i,--rm,-e,TOX_TEST_FILES={TOX_FILES},-e,TOXENV={TOXENV},-v,{FOLDER_MOUNT},{DOCKER_IMAGE_NAME}"
 
     def __init__(self, arguments, test_configuration):
@@ -38,23 +29,59 @@ class IntegrationTestRunner(object):
 
                 args = {
                     "TOXENV": self.python_version,
-                    "TOX_FILES": _compose_test_files(self.test_configuration.test_files),
+                    "TOX_FILES": self._compose_test_files(self.test_configuration.test_files),
                     "FOLDER_MOUNT": ':'.join((self.source_code_folder_path, self.test_configuration.working_directory_docker)),
                     "DOCKER_IMAGE_NAME": env_image['image']
                 }
 
                 docker_command_args = self.docker_command.format(**args).split(',')
+
                 print(docker_command_args)
+
                 title = "Tests for Environment with Image: " + env_image['environment']
                 underline_title = '=' * len(title)
+
                 print(title)
                 print(underline_title)
 
-                for line in _execute_command(docker_command_args):
+                for line in self._execute_command(docker_command_args):
                     print(line, end="")
+
+    @staticmethod
+    def _execute_command(cmd):
+        """
+        Executes command and yields the output-line from the command.
+        If a the command returns a retrun_code the integration-test fails.
+        :param cmd: Command-Args as array.
+        :return: Output of the command.
+        """
+        popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        for stdout_line in iter(popen.stdout.readline, ""):
+            yield stdout_line
+        popen.stdout.close()
+        return_code = popen.wait()
+        if return_code:
+            sys.exit(return_code)
+
+    @staticmethod
+    def _compose_test_files(testing_files):
+        """
+        Compose the testing files defined in IntegrationTestRunnerConfiguration.
+        The testing_files is an array of paths, which must be converted to a string of following format:
+        "tests/integration_test.py tests/integration_test_2.py"
+        :param testing_files: Array of Strings
+        :return: String of paths.
+        """
+        result = ""
+        for file_name in testing_files:
+            result += " " + file_name
+        return result.lstrip()
 
 
 class IntegrationTestRunnerConfiguration(object):
+    """
+    Configuration Wrapper for the IntegrationTestRunner.
+    """
     def __init__(self, working_dir_docker, docker_image_list, testing_files):
         self.working_directory_docker = working_dir_docker
         self.docker_image_list = docker_image_list
@@ -65,18 +92,20 @@ if __name__ == '__main__':
 
     working_directory_docker = "/home/swid"
 
+    # Define environment names and images
     docker_image_names = [
         {"environment": "dpkg", "image": "davidedegiorgio/swidgenerator-dockerimages:debian"},
         {"environment": "rpm", "image": "davidedegiorgio/swidgenerator-dockerimages:redhat"},
         {"environment": "pacman", "image": "davidedegiorgio/swidgenerator-dockerimages:archlinux"}
     ]
 
-    # Relative names of Test-files in working_directory_docker/tests. Multiple Files can be appended.
-    test_files = ['integration_test.py']
+    # Relative paths to testing files in 'working_directory_docker'.
+    # Multiple Files can be appended.
+    test_files = ['tests/integration_test.py']
 
-    integration_test_configuration = IntegrationTestRunnerConfiguration(docker_image_list=docker_image_names,
-                                                                        testing_files=test_files,
-                                                                        working_dir_docker=working_directory_docker)
+    # Define Configuration for IntegrationTestRunner
+    integration_test_configuration = IntegrationTestRunnerConfiguration(working_directory_docker, docker_image_names, test_files)
 
+    # Run
     integration_test = IntegrationTestRunner(sys.argv, integration_test_configuration)
     integration_test.run_main()
