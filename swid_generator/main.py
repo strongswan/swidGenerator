strongswan/swidGenerator

commit 8baf28d0c7740fda869eba153a8f0569d20569b6

Only determine environment if a command is specified
Due to the use of subparsers, the parsed options object is basically empty
if no command is specified, which resulted in an ugly exception when called
without any command line arguments.

swid_generator/main.py
@@ -55,21 +55,21 @@ def main():
     parser = MainArgumentParser(environment_registry)
     options = parser.parse()  # without any parameter it takes arguments passed by command line
 
-    # Get correct environment
-    try:
-        env = environment_registry.get_environment(options.env)
-    except EnvironmentNotInstalledError:
-        print('Error: the given environment is not installed')
-        sys.exit(3)
-    except AutodetectionError:
-        print('Error: Could not autodetect environment.')
-        parser.print_usage()
-        sys.exit(3)
+    def get_environment():
+        try:
+            return environment_registry.get_environment(options.env)
+        except EnvironmentNotInstalledError:
+            print('Error: the given environment is not installed')
+            sys.exit(3)
+        except AutodetectionError:
+            print('Error: Could not autodetect environment.')
+            parser.print_usage()
+            sys.exit(3)
 
     # Handle commands
     if options.command == 'swid':
         swid_args = {
-            'environment': env,
+            'environment': get_environment(),
             'entity_name': options.entity_name,
             'regid': options.regid,
             'os_string': options.os_string,
@@ -142,7 +142,7 @@ def main():
 
     elif options.command == 'software-id':
         sid_args = {
-            'env': env,
+            'env': get_environment(),
             'regid': options.regid,
             'id_prefix': options.id_prefix,
             'dpkg_include_package_arch': options.dpkg_include_package_arch,

commit 9c5a015605344b857590f7766a25fa87c1e0a2ef

Fix determining OS name for targeted (evidence) requests
Fixes: 8baf28d0c774 ("Only determine environment if a command is specified")

swid_generator/main.py
@@ -105,7 +105,7 @@ def main():
             swid_args['full'] = True
 
             if options.name is None:
-                swid_args['name'] = "_".join((unicode_patch(options.evidence_path), env.get_os_string()))
+                swid_args['name'] = "_".join((unicode_patch(options.evidence_path), swid_args['environment'].get_os_string()))
 
             if options.version is None:
                 swid_args['version'] = "1.0.0"